5bf095d64d0f4183c78cd16092ebbe32c3374bba
Stephanie Noirpoudre <stephanie.noirpoudre@gmail.com>
2013-10-28 16:26:54 +0100

    some refactoring

--- arith_eq/arith_eq.rb
+++ arith_eq/arith_eq.rb
@@ -1,6 +1,14 @@
+def get_input_range
+	range = gets.chomp.split(' ')
+	low = range[0].to_i 
+	high = range[1].to_i 
+	low...high
+end
+
 #I think it would be better to split this into smaller methods
-def check_answer_input #this method both prints and checks the answer
-	equation = new_equation2(@range)
+def check_answer_input(range) #this method both prints and checks the answer
+
+	equation = new_equation(range)
 	answer = false
 
 	while answer == false
@@ -13,47 +21,23 @@ def check_answer_input #this method both prints and checks the answer
 			exit
 		elsif answer_user.to_i == result
 			puts "Correct!"
-			check_answer_input
+			check_answer_input(range)
 		else
 			puts "Try again"
 		end
 	end
 end
 
-def new_equation2(range)
+def new_equation(range)
 	equation = ''
 	3.times { equation << "#{rand(range)} #{get_operator} "}
 	equation << rand(range).to_s
 end
 
-#see above for a bit of a shorter way to do the same thing
-def new_equation(range)
-	number1 = rand(range)
-	number2 = rand(range)
-	number3 = rand(range)
-	number4 = rand(range)
-
-	operator1 = get_operator
-	operator2 = get_operator
-	operator3 = get_operator
-
-	"#{number1}  #{operator1} #{number2}  #{operator2} #{number3}  #{operator3} #{number4}"
-end
-
-#because this is called first I would put it as the first method in the file
-def get_input_range
-	range = gets.chomp.split(' ')
-	low = range[0].to_i #I like how you explicitly created low and high variables, makes it clear what the input does.
-	high = range[1].to_i 
-	low...high
-end
-
-#nicely done
 def get_operator
 	operators = ["*", "+", "-"]
 	operators[rand(operators.size)]
 end
 
-#since @range is only used in one other place I would suggest shifting it back to passing as a parameter to check_answer_input
-@range = get_input_range
-check_answer_input
+range = get_input_range
+check_answer_input(range)
