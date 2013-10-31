234c54152e4956ef8cfdfd49eeac03093e945def
Joshua Vial <joshua@enspiral.com>
2013-10-31 13:52:49 +0700

    initial rails install

---
+++ craftworks-info/craftodex/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Craftodex::Application.config.secret_key_base = '0f5c2b87e6afd8b979fdb992d1b10a6adf123e2309d763ca2231dcd722f145953093c0b91fb730ea498f8c6372d238888b5ce90df067f43ea69b614ae6f54a72'
---
+++ craftworks-info/craftodex/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Craftodex::Application.config.session_store :cookie_store, key: '_craftodex_session'
