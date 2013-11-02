fe65e853d49293d3b98fed47ada6289a1312be6d
Stephanie Noirpoudre <stephanie.noirpoudre@gmail.com>
2013-11-02 08:49:59 +0100

    add picture with gravatar

--- craftworks-info/craftodex/app/helpers/application_helper.rb
+++ craftworks-info/craftodex/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def avatar_url(person)
+		#default_url = "#{root_url}assets/images/guest.png"
+     gravatar_id = Digest::MD5.hexdigest(person.email.downcase)
+     #"http://gravatar.com/avatar/#{gravatar_id}.png?s=130&d=#{CGI.escape(default_url)}"
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=140&d=mm"
+  end
 end
--- craftworks-info/craftodex/config/routes.rb
+++ craftworks-info/craftodex/config/routes.rb
@@ -6,7 +6,7 @@ Craftodex::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'people#index'
+    root 'people#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
