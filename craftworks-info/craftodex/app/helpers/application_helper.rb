33949ee403eaf403ebf054dd947d8d9afa280ad4
Stephanie Noirpoudre <stephanie.noirpoudre@gmail.com>
2013-11-08 08:15:28 +0100

    j

--- craftworks-info/craftodex/app/helpers/application_helper.rb
+++ craftworks-info/craftodex/app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
 	def avatar_url(person)
-		#default_url = "#{root_url}assets/images/guest.png"
      gravatar_id = Digest::MD5.hexdigest(person.email.downcase)
-     #"http://gravatar.com/avatar/#{gravatar_id}.png?s=130&d=#{CGI.escape(default_url)}"
      "http://gravatar.com/avatar/#{gravatar_id}.png?s=150&d=mm"
   end
 end
--- craftworks-info/craftodex/app/helpers/projects_helper.rb
+++ craftworks-info/craftodex/app/helpers/projects_helper.rb
@@ -1,2 +1,5 @@
 module ProjectsHelper
+	def link_to_project(project)
+		link_to project.name, projects_path(id:project.id, anchor:project.id)
+	end
 end
